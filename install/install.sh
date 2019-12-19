b6f22e2b12c2be94fc626dda1a0fe180f7e22e0c
Arkadiusz Adach <adach_soft@wp4b.pl>
2019-12-19 06:18:48 +0000

    install

--- install/install.sh
+++ install/install.sh
@@ -2,3 +2,11 @@
 
 echo "INSTALL RC CAR"
 
+if ! [ -x "$(command -v git)" ]; then
+    echo 'git is not installed.'
+    echo 'Install git'
+    sudo apt-get update
+    sudo apt-get install git -y
+fi
+
+git clone git@github.com:adachsoft/RaspberryPi-RC-car.git /var/www/html
