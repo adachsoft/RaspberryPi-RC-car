e98031e034e41a350f270601c7675d756f608ead
Arkadiusz Adach <adach_soft@wp4b.pl>
2019-12-21 07:17:52 +0000

    Install

--- install/install.sh
+++ install/install.sh
@@ -11,9 +11,15 @@ if ! [ -x "$(command -v git)" ]; then
     sudo apt-get install git -y
 fi
 
+sudo rm -R /var/www/html/
+mkdir /var/www/html
+cd /var/www/html
+chown -R pi:pi /var/www/html
+
 git clone https://github.com/adachsoft/RaspberryPi-RC-car.git /var/www/html
 
 cd /var/www/html
+
 
 ./install/install_www.sh
 ./install/install_others.sh
